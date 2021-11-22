e59abc1a55e6a880fc5f791bbac39895874f4964
jh177 <jameshong177@gmail.com>
2021-11-21 17:00:24 -0800

    Bootstrap the current user

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -27,6 +27,26 @@ class SessionForm extends React.Component {
   render() {
     const { formType, link } = this.props
     const title = (formType === "signup") ? "Sign Up" : "Log in"
+    const nameInfo = (formType === "signup") ? (
+      <div>
+        <label>First Name:
+          <input
+            type="text"
+            value={this.state.fname}
+            onChange={this.handleInput('fname')}
+          />
+        </label>
+        <br />
+        <label>Last Name:
+          <input
+            type="text"
+            value={this.state.lname}
+            onChange={this.handleInput('lname')}
+          />
+        </label>
+        <br />
+      </div>) : null;
+
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
@@ -50,22 +70,7 @@ class SessionForm extends React.Component {
             />
           </label>
           <br />
-          <label>First Name:
-            <input
-              type="text"
-              value={this.state.fname}
-              onChange={this.handleInput('fname')}
-            />
-          </label>
-          <br />
-          <label>Last Name:
-            <input
-              type="text"
-              value={this.state.lname}
-              onChange={this.handleInput('lname')}
-            />
-          </label>
-          <br />
+          {nameInfo}
           <button>Submit</button>
         </form>
       </div>
