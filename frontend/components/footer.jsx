5086d96e0b32421015f98c9b9876180b0ff9ada7
jh177 <jameshong177@gmail.com>
2022-01-07 15:54:06 -0800

    Edit portfolio site link

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return(
     <div className="footer-content">
       <div className="footer-links">
-        <a href="https://jh177.github.io/portfolio/" target="_blank">
+        <a href="http://www.jameszhong.com/" target="_blank">
           <img src={window.portfolioUrl} id="portfolio-logo"/>
         </a>
         <a href={`https://github.com/jh177`} target="_blank">
